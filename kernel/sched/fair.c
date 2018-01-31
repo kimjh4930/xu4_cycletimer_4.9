74f5e3392e4c144150b42fa06d7922260966a4ca
junha <kimjh4930@gmail.com>
2018-01-31 09:01:28 +0000

    modify exec_start_cycle

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -1194,6 +1194,7 @@ update_stats_curr_start(struct cfs_rq *cfs_rq, struct sched_entity *se)
 	 * We are starting a new run period:
 	 */
 	se->exec_start = rq_clock_task(rq_of(cfs_rq));
+	se->exec_start_cycle = read_cycles();
 }
 
 /**************************************************
